3c56ce262af7e5f5ce928e828bf9808b520cf7e9
newbpydev <newbpydev@gmail.com>
2022-03-08 11:59:10 -0400

    bug: webpack duplicate

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,19 @@
 //* Entry -> output
 const path = require("path");
-const webpack = require("webpack")
+const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { webpack } = require("webpack");
+// const { webpack } = require("webpack");
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 if (process.env.NODE_ENV === "test") {
-  require('dotenv').config({path: ".env.test"})
+  require("dotenv").config({ path: ".env.test" });
 } else if (process.env.NODE_ENV === "development") {
-  require('dotenv').config({ path: ".env.development"})
+  require("dotenv").config({ path: ".env.development" });
 }
 
 module.exports = function (env, argv) {
   const isProduction = argv.mode === "production";
-  
 
   return {
     // target: "node",
@@ -68,14 +67,28 @@ module.exports = function (env, argv) {
         filename: "styles.css",
       }),
       new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
-        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID)
-      })
+        "process.env.FIREBASE_API_KEY": JSON.stringify(
+          process.env.FIREBASE_API_KEY
+        ),
+        "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
+          process.env.FIREBASE_AUTH_DOMAIN
+        ),
+        "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
+          process.env.FIREBASE_DATABASE_URL
+        ),
+        "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
+          process.env.FIREBASE_PROJECT_ID
+        ),
+        "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
+          process.env.FIREBASE_STORAGE_BUCKET
+        ),
+        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
+          process.env.FIREBASE_MESSAGING_SENDER_ID
+        ),
+        "process.env.FIREBASE_APP_ID": JSON.stringify(
+          process.env.FIREBASE_APP_ID
+        ),
+      }),
     ],
     devtool: isProduction ? "source-map" : "inline-cheap-module-source-map",
     devServer: {
@@ -100,7 +113,6 @@ module.exports = function (env, argv) {
     },
   };
 };
-
 
 // console.log(path.join(__dirname, 'public'))
 
