292d350ea198cb7f71fefa024b9a5ad05d8d16b7
newbpydev <newbpydev@gmail.com>
2022-03-10 18:44:39 -0400

    style: add styles to expense list and items

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -4,16 +4,21 @@ import numeral from "numeral";
 
 import { Link } from "react-router-dom";
 
-const ExpenseListItem = ({id, description, amount, createdAt, expenses, dispatch }) => (
-  <div>
-    <h3><Link to={`/edit/${id}`} state={{id}}>{description}</Link></h3>
-    <p>
-      Amount: {numeral(amount / 100).format("$0,0.00")}
-      -
-      Created At: {moment(createdAt).format('MMMM Do, YYYY')}
-    </p>
-    
-  </div>
+const ExpenseListItem = ({
+  id,
+  description,
+  amount,
+  createdAt,
+  expenses,
+  dispatch,
+}) => (
+  <Link className="list-item" to={`/edit/${id}`} state={{ id }}>
+    <div>
+      <h3 className="list-item__title">{description}</h3>
+      <span className="list-item__sub-title">{moment(createdAt).format("MMMM Do, YYYY")}</span>
+    </div>
+    <h3 className="list-item__data">{numeral(amount / 100).format("$0,0.00")}</h3>
+  </Link>
 );
 
 // const mapStateToProps = (state) => {
